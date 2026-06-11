9b8c00ba34fe74b2cfb6131018e8ae4be0a93869
BazovicV <bazovicv@gmail.com>
2026-06-11 11:19:58 +0200

    Cleaning up and added simple movement

--- game.py
+++ game.py
@@ -23,48 +23,64 @@ class Game(tk.Tk):
         for row, row_of_nums in enumerate(game_map):
             for column, true_false in enumerate(row_of_nums):
                 if true_false == 1:
-                    if row == 0 and column == 0:
-                        self.zero_x0, self.zero_y0, self.zero_x1, self.zero_y1 = 10, 10, 50, 50
-                        starting_wall = self.wall.create_rectangle(self.zero_x0, self.zero_y0, self.zero_x1, self.zero_y1, fill='white')
-                        starting_wall
-                        
-                    else:
-                        try:
-                            x0 = 40
-                            zero_check = 10 / column
-
-                        except ZeroDivisionError:
-                            self.zero_y0 += 40
-                            self.zero_y1 += 40
-                            self.zero_x0 = 10
-                            self.zero_x1 = 50
-                            x0 = 0
-
-                        finally:
-                            self.wall.create_rectangle(self.zero_x0+x0, self.zero_y0, self.zero_x1+x0, self.zero_y1, fill='white')
-                            self.zero_x0, self.zero_y0, self.zero_x1, self.zero_y1 = self.zero_x0+x0, self.zero_y0, self.zero_x1+x0, self.zero_y1
-
+                    colour = 'white'
+                
                 if true_false == 0:
-                    if row == 0 and column == 0:
-                        self.zero_x0, self.zero_y0, self.zero_x1, self.zero_y1 = 10, 10, 50, 50
-                        starting_wall = self.wall.create_rectangle(self.zero_x0, self.zero_y0, self.zero_x1, self.zero_y1, fill='blue')
-                        starting_wall
+                    colour = 'black'
+
+                if row == 0 and column == 0:
+                    self.start_x0, self.start_y0, self.finish_x1, self.finish_y1 = 10, 10, 50, 50
+                    starting_wall = self.wall.create_rectangle(self.start_x0, self.start_y0, self.finish_x1, self.finish_y1, fill=colour)
+                    starting_wall
                         
+                else:   
+                    if column == 0:
+                        self.start_y0 += 40
+                        self.finish_y1 += 40
+                        self.start_x0 = 10
+                        self.finish_x1 = 50
+                        x0 = 0
+
                     else:
-                        try:
-                            x0 = 40
-                            zero_check = 10 / column
+                        x0 = 40
+                    
+                    self.wall.create_rectangle(self.start_x0+x0, self.start_y0, self.finish_x1+x0, self.finish_y1, fill=colour)
+                    self.start_x0, self.start_y0, self.finish_x1, self.finish_y1 = self.start_x0+x0, self.start_y0, self.finish_x1+x0, self.finish_y1
 
-                        except ZeroDivisionError:
-                            self.zero_y0 += 40
-                            self.zero_y1 += 40
-                            self.zero_x0 = 10
-                            self.zero_x1 = 50
-                            x0 = 0
+        self.player_x0, self.player_y0, self.player_x1, self.player_y1 = 60, 60, 70, 70
+        self.player = self.wall.create_rectangle(self.player_x0, self.player_y0, self.player_x1, self.player_y1, fill='red')
+        self.player
 
-                        finally:
-                            self.wall.create_rectangle(self.zero_x0+x0, self.zero_y0, self.zero_x1+x0, self.zero_y1, fill='blue')
-                            self.zero_x0, self.zero_y0, self.zero_x1, self.zero_y1 = self.zero_x0+x0, self.zero_y0, self.zero_x1+x0, self.zero_y1
+        for key in 'wasd':
+            self.bind(key, lambda event, k=key: self.player_movement(k))
+
+    def player_movement(self, k): # USE MOVE INSTEAD
+        if k == 'w':
+            self.player_x0 += 0
+            self.player_y0 -= 2
+            self.player_x1 += 0
+            self.player_y1 -= 2
+           
+        if k == 'a':
+            self.player_x0 -= 2
+            self.player_y0 += 0
+            self.player_x1 -= 2
+            self.player_y1 += 0
+
+        if k == 's':
+            self.player_x0 += 0
+            self.player_y0 += 2
+            self.player_x1 += 0
+            self.player_y1 += 2
+
+        if k == 'd':
+            self.player_x0 += 2
+            self.player_y0 += 0
+            self.player_x1 += 2
+            self.player_y1 += 0 
+
+        self.wall.delete(self.player)
+        self.player = self.wall.create_rectangle(self.player_x0, self.player_y0, self.player_x1, self.player_y1, fill='red')   
 
 
 
